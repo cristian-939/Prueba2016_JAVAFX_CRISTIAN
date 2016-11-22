2e95b0b5f14ec6cf6370f555a0c28052e138b41a
cristian <crystyan-vaquero@hotmail.com>
2016-11-22 18:25:23 +0100

    Comit inicial

---
+++ PRUEBA_JAVAFX_CRISTIAN/src/application/control/ControlFX.java
@@ -0,0 +1,72 @@
+package application.control;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControlFX {
+
+	// not using yet
+	@FXML
+	private ImageView imageRaw;
+
+	// Reference to the main application
+	private MainFX mainApp;
+
+	/**
+	 * Es llamado por el main la aplicacion para poder traer referencias a ella
+	 * misma
+	 * 
+	 * @param mainApp
+	 *            que recibe datos de Main
+	 * @version 1.0
+	 */
+	public void setMainApp(MainFX mainApp) {
+		this.mainApp = mainApp;
+	}
+
+	/**
+	 * @author Cristian Este metodo lo que hace es enseñar la Send.fxml
+	 */
+	public void sendStage() {
+		try {
+			// Load the fxml file and create a new stage for the popup.
+			FXMLLoader loader = new FXMLLoader();
+			// loader.setLocation(Main.class.getResource("view/Paginafoto.fxml"));
+			loader.setLocation(MainFX.class.getResource("../vista/Send.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Stage sendStage = new Stage();
+			sendStage.setTitle("PaginaBOMBA");
+			Scene scene = new Scene(page);
+			sendStage.setScene(scene);
+
+			sendStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @author Cristian Este metodo anda Send.fxml para verla
+	 * @see sendStage
+	 */
+	@FXML
+	private void handleSend() {
+		sendStage();
+	}
+
+	/**
+	 * Este metodo cierra la aplicaccion
+	 */
+	@FXML
+	private void closeStage() {
+		System.exit(0);
+	}
+
+}
---
+++ PRUEBA_JAVAFX_CRISTIAN/src/application/control/MainFX.java
@@ -0,0 +1,73 @@
+package application.control;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * @author Cristian
+	 * @param primaryStage
+	 * establece un constructor y lo guarda Inicializa con start y
+	 * recibe la informacion de Stage
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("My GitHub Profile");
+		initRootLayout();
+
+	}
+
+	/**
+	 * @author Cristian
+	 * @serialData v.1.0
+	 * Inicia la vista en la cual mete valores y se establece
+	 *         una conexion con el css
+	 *         
+	 */
+	private void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFX.class.getResource("../vista/Profile.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			// ------------------------------------BLOQUE DE LINCAMIENTO
+			// CSS--------------
+
+			// ponerle fuentes
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// enlazo con el css
+			Font.loadFont(getClass().getResourceAsStream("../vista/assets/fonts/HipsterishFontNormal.ttf"), 20);
+
+			// ------------------------------------BLOQUE DE LINCAMIENTO
+			// GOOGLE--------------
+			// scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Shadows+Into+Light");
+			// scene.getStylesheets().add("https://fonts.googleapis.com/css?family=Bungee+Inline");
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
